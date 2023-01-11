8bf3ae1cb53feaf19cb1ab4dade12b8faf03e9c1
Ariannebez <72392619+Ariannebez@users.noreply.github.com>
2023-01-11 11:28:15 +0100

    Js code changed

--- ColdPlay/assets/javascript/conscript.js
+++ ColdPlay/assets/javascript/conscript.js
@@ -1,51 +1,47 @@
-function clearerrors(){
-    
-    document.getElementById('errors-content').innerHTML="";
-      
-    document.getElementById("errors").classList.remove('d-block');
 
-    document.getElementById("errors").classList.add('d-none');
-    
+function clearErrors(){
+    document.getElementById('errors-content').innerHTML = 
+    "";
 
-    
+    document.getElementById('errors').classList.remove('d-block');
 
+    document.getElementById('errors').classList.add('d-none');
 }
 
 function validateForm(){
-    clearerrors();
 
+    clearErrors();
+    
     var firstName = document.getElementById('firstName').value;
     var lastName = document.getElementById('lastName').value;
     var email = document.getElementById('email').value;
     var message = document.getElementById('message').value;
 
     var errors = "";
-    
+
     if(isEmpty(firstName) || 
-        isEmpty(lastName) || 
-        isEmpty(email)|| 
+        isEmpty(lastName) ||
+        isEmpty(email) ||
         isEmpty(message)){
             errors += "<li>Please fill in all fields.</li>";
     }
 
-    if(
-        short(message)){
-            errors+="<li>The message is to short, please fill in a valid message with 20 characters  </li>";
-    
-        
-       }
-    
+    if (isTooShort(message)){
+        errors += "<li>Message too short, please provide more details.</li>";
+    }
 
-    if(errors != ""){
-        document.getElementById('errors-content').innerHTML = "<ul class='errors-list'>"+errors+"</ul>";
-        /*Removing the d-none to make the card show when having errors*/
+
+    if (errors != ""){
+        document.getElementById('errors-content').innerHTML = 
+        "<ul class='errors-list'>"+errors+"</ul>";
+
         document.getElementById('errors').classList.remove('d-none');
 
         document.getElementById('errors').classList.add('d-block');
 
         return false;
     }
-    else{
+    else {
         return true;
     }
 }
@@ -59,14 +55,11 @@ function isEmpty(field){
     }
 }
 
-   function short(field){
-
-    if (field.length<20){
+function isTooShort(field){
+    if(field.length < 20){
         return true;
-
     }
-    else{
+    else {
         return false;
     }
-
 }
